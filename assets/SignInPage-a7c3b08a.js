dd8c353cf303ac26bddbd975f9b1026be949900c
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2026-03-21 20:52:37 +0000

    Deploying to gh-pages from @ summermoved0n/project-01@178a36877bc93ca371b4500794a680383344bdd4 🚀

---
+++ assets/SignInPage-a7c3b08a.js
@@ -0,0 +1,77 @@
+import{b as r,g as a,j as i}from"./index-e1c93223.js";import{n as o,m as n,a as t,b as d,t as p,c as s,d as x,e as l,f as m,g as u,S as g,A as c}from"./SignUpPage.styled-2a4b8517.js";const b=o.div`
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 60px;
+  left: 0;
+  z-index: -10;
+  background-image: url(${n}), url(${t});
+  background-size: 100%, cover;
+  background-repeat: no-repeat;
+  background-position:
+    top 400px left 20px,
+    center;
+
+  @media (min-device-pixel-ratio: 2),
+    (-webkit-min-device-pixel-ratio: 2),
+    (min-resolution: 192dpi),
+    (min-resolution: 2dppx) {
+    background-image: url(${d}), url(${t});
+  }
+
+  @media (min-width: 768px) {
+    margin: 0 auto;
+    max-width: 768px;
+    width: 100%;
+    height: 100vh;
+    top: -20px;
+    left: 0;
+    right: 30px;
+    background-image: url(${p});
+    background-size: cover;
+    background-position: center;
+
+    @media (min-device-pixel-ratio: 2),
+      (-webkit-min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url(${s});
+    }
+  }
+
+  @media (min-width: 1440px) {
+    margin: 0 auto;
+    max-width: 1404px;
+    width: 100%;
+    height: 100vh;
+    top: 60px;
+    left: 18px;
+    background-image: url(${x}), url(${l});
+    background-size: 916px, 1404px;
+    background-position:
+      top 28px left -51px,
+      center;
+
+    @media (min-device-pixel-ratio: 2),
+      (-webkit-min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url(${m}), url(${u});
+    }
+  }
+`,h=o.p`
+  margin-top: 20px;
+  width: 280px;
+  font-size: 18px;
+  text-align: center;
+  color: var(--secondary-color-red);
+  @media (min-width: 768px) {
+    width: 320px;
+    margin-right: auto;
+    font-size: 20px;
+  }
+  @media (min-width: 1440px) {
+    margin-left: auto;
+    margin-right: 133px;
+  }
+`,v=()=>{const e=r(a);return i.jsxs(g,{children:[i.jsx(b,{}),i.jsx(c,{}),e&&i.jsx(h,{children:e})]})};export{v as default};
